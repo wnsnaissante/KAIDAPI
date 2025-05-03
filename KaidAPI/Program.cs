2c4b40dd5754776559fb88049f9e6b14cf173d93
wnsnaissante <wnsnaissante@gmail.com>
2025-05-03 14:07:17 +0900

    Init

---
+++ KaidAPI/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace KaidAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+        
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect(options =>
+            {
+                var oidcConfig = builder.Configuration.GetSection("OpenIDConnectSettings");
+                options.RequireHttpsMetadata = false;
+                
+                options.BackchannelHttpHandler = new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                };
+                options.Authority = oidcConfig["Authority"];
+                options.ClientId = oidcConfig["ClientId"];
+                options.ClientSecret = oidcConfig["ClientSecret"];
+
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.ResponseType = OpenIdConnectResponseType.Code;
+                
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("email");
+                options.SaveTokens = true;
+                
+                options.CallbackPath = "/signin-oidc"; 
+                options.SignedOutCallbackPath = "/signout-callback-oidc"; 
+                options.GetClaimsFromUserInfoEndpoint = true;
+
+                options.MapInboundClaims = false;
+                options.TokenValidationParameters.NameClaimType = JwtRegisteredClaimNames.Name;
+                options.TokenValidationParameters.RoleClaimType = "roles";
+            });
+        
+        var app = builder.Build();
+        
+        if (app.Environment.IsDevelopment())
+        {
+
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        
+        app.MapGet("/", [Authorize] () => "Hello, Authorized World!");
+        app.Run();
+    }
+}
