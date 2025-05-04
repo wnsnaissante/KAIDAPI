315eb718da01853d0012d0a2e3511b597384a89a
wnsnaissante <wnsnaissante@gmail.com>
2025-05-05 03:50:05 +0900

    user info implemented

---
+++ KaidAPI/Controllers/UserInfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KaidAPI.Controllers;
+
+[ApiController]
+[Authorize]
+public class UserInfoController : ControllerBase
+{
+    [HttpGet] 
+    [Route("api/userinfo")]
+    public IActionResult GetUserInfo()
+    {
+        var user = HttpContext.User;
+        var name = user.FindFirst("name")?.Value ?? "'name' claim not found";
+        var email = user.FindFirst("email")?.Value ?? "'email' claim not found";
+        var sub = user.FindFirst("sub")?.Value ?? "'sub' claim not found";
+        return Ok(new
+        {
+            name,
+            email, 
+            sub,
+        });
+    }
+}
