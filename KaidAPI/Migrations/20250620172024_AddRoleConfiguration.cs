45a8dd506e45b03c9035762621d362134cec362c
wnsnaissante <ind3pe@gmail.com>
2025-06-21 02:20:41 +0900

    feat: Add RoleConfiguration migration with initial role data for role management

---
+++ KaidAPI/Migrations/20250620172024_AddRoleConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace KaidAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoleConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "RoleDescription", "RoleName" },
+                values: new object[,]
+                {
+                    { 1, "Project Administrator", "Admin" },
+                    { 2, "Team Manager", "Manager" },
+                    { 3, "Team Member", "Member" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 3);
+        }
+    }
+}
