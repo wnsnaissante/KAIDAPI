5ba20ba8297149606d6bf9f14c08857367a54f6c
이광수 <qwe402700@naver.com>
2025-06-22 20:29:53 +0900

    fix: Add namespace to Membership Table
    - Add namespace to Membership Table

--- KaidAPI/Models/Membership.cs
+++ KaidAPI/Models/Membership.cs
@@ -3,37 +3,40 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using KaidAPI.Models;
 
-public class Membership
+namespace KaidAPI.Models
 {
-    [Key]
-    public Guid ProjectMembershipId { get; set; }
-    [Required]
-    public Guid ProjectId { get; set; }
-    [Required]
-    public Guid UserId { get; set; }
-    public Guid? SuperiorId { get; set; }
-    [Required]
-    public int RoleId { get; set; }
-    
-    public bool IsActivated { get; set; }
+    public class Membership
+    {
+        [Key]
+        public Guid ProjectMembershipId { get; set; }
+        [Required]
+        public Guid ProjectId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        public Guid? SuperiorId { get; set; }
+        [Required]
+        public int RoleId { get; set; }
 
-    public Guid? TeamId { get; set; }
-    public DateTime JoinedAt { get; set; }
+        public bool IsActivated { get; set; }
 
-    [ForeignKey("RoleId")]
-    public Role Role { get; set; }
+        public Guid? TeamId { get; set; }
+        public DateTime JoinedAt { get; set; }
 
-    [ForeignKey("ProjectId")]
-    public Project Project { get; set; }
-    
-    [ForeignKey("TeamId")]
-    public Team Team { get; set; }
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
 
-    [ForeignKey("SuperiorId")]
-    public User Superior { get; set; }
-    
-    [Required]
-    [MaxLength(20)]
-    public string Status { get; set; }
+        [ForeignKey("ProjectId")]
+        public Project Project { get; set; }
+
+        [ForeignKey("TeamId")]
+        public Team Team { get; set; }
+
+        [ForeignKey("SuperiorId")]
+        public User Superior { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Status { get; set; }
+    }
 }
 
